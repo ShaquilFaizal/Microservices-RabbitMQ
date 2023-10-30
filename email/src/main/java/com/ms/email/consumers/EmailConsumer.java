5536800adef0bae55037f113f0d9389ae96337fd
Shaquil F. Daude <shaquil.faizal@gmail.com>
2023-10-30 19:12:01 +0200

    feat[email]: Added possibility to send emails and save to db

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,15 +1,26 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import services.EmailService;
 
 @Component
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+
+    final EmailService emailService;
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto,emailModel);
+        emailService.sendEmail(emailModel);
+
     }
 }
